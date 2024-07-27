f2983f134f24837eed883ce311779a817510a576
khc <khe4oyan@gmail.com>
2024-07-27 19:18:46 +0400

    s

--- src/components/ShowTodosWithFilters/ShowTodosWithFilters.jsx
+++ src/components/ShowTodosWithFilters/ShowTodosWithFilters.jsx
@@ -13,9 +13,9 @@ export default function ShowTodosWithFilters({ selectedFilters, todos }) {
   } else {
     for (let i = 0; i < todos.length; ++i) {
       const todo = todos[i];
-      const { todoStatus, category, priority } = todo;
+      const { statusIsDone, category, priority } = todo;
       if (
-        selectedFilters.includes(todoStatus) ||
+        selectedFilters.includes(statusIsDone) ||
         selectedFilters.includes(category) ||
         selectedFilters.includes(priority)
       ) {
@@ -26,15 +26,16 @@ export default function ShowTodosWithFilters({ selectedFilters, todos }) {
 
   return (
     <Flex wrap vertical style={{ margin: "10px 0" }} gap="10px">
-      {
-        filteredTodos.length < 1 ? (
-          <Empty description="Add your first todo!" image={Empty.PRESENTED_IMAGE_SIMPLE} />
-        ) : (
-          filteredTodos.map((todo, i) => (
-            <TodoCard key={i} todoData={todo} ind={i} />
-          ))
-        )
-      }
+      {filteredTodos.length < 1 ? (
+        <Empty
+          description="Add your first todo!"
+          image={Empty.PRESENTED_IMAGE_SIMPLE}
+        />
+      ) : (
+        filteredTodos.map((todo, i) => (
+          <TodoCard key={i} todoData={todo} ind={i} />
+        ))
+      )}
     </Flex>
   );
 }
