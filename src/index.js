6b523824c53e27bc8e67a6883a35bfd4dba369db
khc <khe4oyan@gmail.com>
2024-07-28 21:58:02 +0400

    change BrowserRouter to HashRouter for work in GitHub pages

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // libs
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { createGlobalStyle } from "styled-components";
 
@@ -58,9 +58,9 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
 
     <GlobalStyles />
